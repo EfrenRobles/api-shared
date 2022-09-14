f3e180aef37202504f9581a7dab354ed39a4f246
Efren Robles <efren_robles@hotmail.com>
2022-09-14 15:12:17 -0700

    + Adding the format base for the response by the controllers.

---
+++ src/main/java/api/carreras/shared/domain/response/OnResponse.java
@@ -0,0 +1,29 @@
+package api.carreras.shared.domain.response;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public abstract class OnResponse {
+
+    public static ResponseEntity<Object> onSuccess(Object result, HttpStatus status) {
+
+        return response(result, status, ResponseEnum.SUCCESS, "data");
+    }
+
+    public static ResponseEntity<Object> onError(Object result, HttpStatus status) {
+
+        return response(result, status, ResponseEnum.ERROR, "error");
+    }
+
+    private static ResponseEntity<Object> response(Object result, HttpStatus status, ResponseEnum tagStatus, String tagType) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("status", tagStatus);
+        response.put(tagType, result);
+
+        return new ResponseEntity<>(response, status);
+    }
+
+}
