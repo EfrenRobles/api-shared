b11e694a66a066dcec22a55b347a8d793f4bf1e0
Efren Robles <efren_robles@hotmail.com>
2022-09-14 15:11:49 -0700

    + Adding handle exceptions.

---
+++ src/main/java/api/carreras/shared/domain/exception/ServiceException.java
@@ -0,0 +1,10 @@
+package api.carreras.shared.domain.exception;
+
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ServiceException(String exception) {
+        super(exception);
+    }
+}
