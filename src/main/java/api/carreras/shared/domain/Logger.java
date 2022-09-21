8d836b83f83ddcaa06884be1c9ed7bb7611d3cab
Efren Robles <efren_robles@hotmail.com>
2022-09-21 09:02:22 -0700

    * Updating Logger to debug only on local environment.

--- src/main/java/api/carreras/shared/domain/Logger.java
+++ src/main/java/api/carreras/shared/domain/Logger.java
@@ -1,11 +1,17 @@
 package api.carreras.shared.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Logger {
 
-    public static void log(Object input) {
-        System.out.println(" -------------------------------------------- ");
-        System.out.println(input);
-        System.out.println(" -------------------------------------------- ");
+    @Value("${spring.profiles.active}")
+    private static String env;
 
+    public static void log(Object input) {
+        if (env.equalsIgnoreCase("local")) {
+            System.out.println(" -------------------------------------------- ");
+            System.out.println(input);
+            System.out.println(" -------------------------------------------- ");
+        }
     }
 }
