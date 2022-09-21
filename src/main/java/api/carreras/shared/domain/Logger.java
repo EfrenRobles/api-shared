b97128d310f71f920e4879ee9f01a27d85efc0e0
Efren Robles <efren_robles@hotmail.com>
2022-09-21 11:55:39 -0700

    + Adding EnvConfig to handle the application's properties. * Fixing issue with flag on Logger.
    (cherry picked from commit ce89a8314d84c47878b5193507245da0f1fe4096)

--- src/main/java/api/carreras/shared/domain/Logger.java
+++ src/main/java/api/carreras/shared/domain/Logger.java
@@ -1,14 +1,11 @@
 package api.carreras.shared.domain;
 
-import org.springframework.beans.factory.annotation.Value;
+import api.carreras.shared.infrastructure.config.EnvConfig;
 
 public class Logger {
 
-    @Value("${spring.profiles.active}")
-    private static String env;
-
     public static void log(Object input) {
-        if (env.equalsIgnoreCase("local")) {
+        if (EnvConfig.getProperty("spring.profiles.active").equalsIgnoreCase("local")) {
             System.out.println(" -------------------------------------------- ");
             System.out.println(input);
             System.out.println(" -------------------------------------------- ");
