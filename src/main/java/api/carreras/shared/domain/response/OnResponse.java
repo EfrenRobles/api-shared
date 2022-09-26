28a541900d4626de06e331441ccf1dac89b304e2
Efren Robles <efren_robles@hotmail.com>
2022-09-26 13:06:15 -0700

    + Adding Pagination to handle any query that needs a pagination process. * Removing ObjectMapper from OnResponse some issues with Date type. * Adding status var on PaginationResponse due the issues with ObjectMapper. * Updating some constants name from PaginationConstant.

--- src/main/java/api/carreras/shared/domain/response/OnResponse.java
+++ src/main/java/api/carreras/shared/domain/response/OnResponse.java
@@ -1,25 +1,15 @@
 package api.carreras.shared.domain.response;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public abstract class OnResponse {
 
-    @SuppressWarnings("unchecked")
-    public static ResponseEntity<Object> onSuccessPagination(Object result, HttpStatus status) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> temp = objectMapper.convertValue(result, LinkedHashMap.class);
-
-        Map<String, Object> response = new LinkedHashMap<>();
-        response.put("status", ResponseEnum.SUCCESS);
-        response.putAll(temp);
-
-        return new ResponseEntity<>(response, status);
+    public static ResponseEntity<PaginationResponse> onSuccessPagination(PaginationResponse result, HttpStatus status) {
+        
+        return new ResponseEntity<>(result, status);
     }
 
     public static ResponseEntity<Object> onSuccess(Object result, HttpStatus status) {
@@ -32,9 +22,12 @@ public abstract class OnResponse {
         return response(result, status, ResponseEnum.ERROR, "error");
     }
 
-    private static
-        ResponseEntity<Object>
-        response(Object result, HttpStatus status, ResponseEnum tagStatus, String tagType) {
+    private static ResponseEntity<Object> response(
+        Object result,
+        HttpStatus status,
+        ResponseEnum tagStatus,
+        String tagType
+    ) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("status", tagStatus);
         response.put(tagType, result);
