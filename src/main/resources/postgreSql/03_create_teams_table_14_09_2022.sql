ba8138f14d88dc7c0bb6680ffd1d208d8a45f8a5
Efren Robles <efren_robles@hotmail.com>
2022-09-27 08:59:22 -0700

    - Removing more files and folders not needed.

--- src/main/resources/postgreSql/03_create_teams_table_14_09_2022.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE public.teams (
-    team_id bigint NOT NULL,
-    team_name varchar(250) DEFAULT NULL,
-    CONSTRAINT teams_pk PRIMARY KEY (team_id)
-);
--- src/main/resources/postgreSql/06_create_team_drivers_table_14_09_2022.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.team_drivers (
-    team_id bigint NOT NULL,
-    driver_id bigint NOT NULL,
-    CONSTRAINT team_drivers_pk PRIMARY KEY (driver_id,team_id),
-    CONSTRAINT team_drivers_FK FOREIGN KEY (driver_id) REFERENCES drivers(driver_id) ON DELETE CASCADE,
-    CONSTRAINT team_drivers_FK_1 FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE
-);
