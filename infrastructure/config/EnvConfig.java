0eda5526aed52618c3ac67934279e7a5f63a2677
Efren Robles <efren_robles@hotmail.com>
2022-10-09 11:27:02 -0700

    * Updating SecurityConfig, to handle the Authentication event.

--- infrastructure/config/EnvConfig.java
+++ infrastructure/config/EnvConfig.java
@@ -2,23 +2,26 @@ package api.shared.infrastructure.config;
 
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import api.shared.domain.Logger;
+
 public class EnvConfig {
 
     private ConfigurableEnvironment env;
-    
+
     private static EnvConfig instance = new EnvConfig();
-    
+
     public static void setProperty(ConfigurableEnvironment env) {
+
         instance.env = env;
     }
-    
-    public static  String getProperty(String data) {
+
+    public static String getProperty(String data) {
         String result = instance.env.getProperty(data);
-        
+
         if (result == null) {
             return "";
         }
-        
+
         return result;
     }
 }
--- infrastructure/config/SecurityConfig.java
+++ infrastructure/config/SecurityConfig.java
@@ -1,36 +1,30 @@
 package api.shared.infrastructure.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-//    private final UserDetailsService userDetailsService;
-//
-//    public SecurityConfig(UserDetailsService userDetailsService) {
-//        super();
-//        this.userDetailsService = userDetailsService;
-//    }
+    @Autowired
+    DataSource dataSource;
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    UserDetailsManager userDetailsManager() {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+
+        return userDetailsManager;
     }
 
     @Bean
@@ -40,39 +34,4 @@ public class SecurityConfig {
 
         return authenticationConfiguration.getAuthenticationManager();
     }
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-            .csrf()
-            .disable()
-            .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/api/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
-
-        return http.build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-            .username("user")
-            .password(passwordEncoder().encode("password"))
-            .roles("USER")
-            .build();
-
-        UserDetails admin = User.builder()
-            .username("admin")
-            .password(passwordEncoder().encode("password"))
-            .roles("ADMIN")
-            .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
 }
